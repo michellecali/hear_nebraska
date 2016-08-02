4fa5c9010c81ba2aeb1e2ee955c238b6f64c18c2
Michelle Cali <mctexx@gmail.com>
2016-08-02 15:55:56 -0500

    finished with issue 3

--- global.js
+++ global.js
@@ -61,12 +61,74 @@ window.addEventListener("load", function(){
 
   });
    
-}); 
+
+  anotherMember.addEventListener("click", newMemberInstrument);
   
+   function newMemberInstrument(element) {  
+      
+      var member = document.getElementsByClassName("memberInstrumentForm__member");
+      var instrument = document.getElementsByClassName("memberInstrumentForm__instrument");
+      var n = member.length;
+      member[n-1].style.display = "none";
+      instrument[n-1].style.display = "none";
 
+      
+      var newMember = document.createElement("INPUT");
+      newMember.setAttribute("type", "text");
+      newMember.setAttribute("class", "memberInstrumentForm__member");
+      newMember.setAttribute("placeholder", "Name");
+      newMember.setAttribute("id", "member" + n);
+      newMember.setAttribute("style", "display:block");
 
+      var newInstrument = document.createElement("INPUT");
+      newInstrument.setAttribute("type", "text");
+      newInstrument.setAttribute("class", "memberInstrumentForm__instrument");
+      newInstrument.setAttribute("placeholder", "Instrument(s)");
+      newInstrument.setAttribute("id", "instrument" + n);
+      newInstrument.setAttribute("style", "display:block");
 
+      memberInstrumentForm.appendChild(newMember);
+      memberInstrumentForm.appendChild(newInstrument);
 
+      var newMembersDiv = document.createElement("div");
+      newMembersDiv.setAttribute("class", "Members");
 
+      var newInstrumentsDiv = document.createElement("div");
+      newInstrumentsDiv.setAttribute("class", "Instruments");
 
+      memberInstrumentPage.appendChild(newMembersDiv);
+      memberInstrumentPage.appendChild(newInstrumentsDiv);
 
+      lastMember.style.display="block";
+      
+    };
+        
+
+    submitBandButton.addEventListener("click", function(){
+      var member = document.getElementsByClassName("memberInstrumentForm__member");
+      var instrument = document.getElementsByClassName("memberInstrumentForm__instrument");
+      var membersDiv = document.getElementsByClassName("Members");
+      var instrumentsDiv = document.getElementsByClassName("Instruments");
+
+      for (var i = 0; i < member.length; i++) {
+      membersDiv[i].innerHTML = member[i].value;
+      }
+      
+      for (var i = 0; i < instrument.length; i++) {
+      instrumentsDiv[i].innerHTML = instrument[i].value;
+      }
+      
+      memberInstrumentForm.style.display = "none";
+      lastMember.style.display="none";
+      homePage.style.display="block";
+
+    });
+
+     
+
+    goToTitleArtistForm.addEventListener("click", function(){
+        memberInstrumentForm.style.display = "none";
+        titleArtistForm.style.display="block";
+    });
+
+}); 
